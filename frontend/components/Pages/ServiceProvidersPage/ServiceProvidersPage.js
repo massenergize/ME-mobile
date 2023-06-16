01e4712c2f5b1478190c4cb311de696f668cd82a
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2023-06-16 11:28:01 -0400

    Update service providers layout

--- frontend/components/Pages/ServiceProvidersPage/ServiceProvidersPage.js
+++ frontend/components/Pages/ServiceProvidersPage/ServiceProvidersPage.js
@@ -10,180 +10,71 @@ import {
 } from "native-base";
 
 export default function ServiceProvidersPage() {
+  const generateSProvider = () => {
+    let sProvider = [];
+    for (let i = 0; i < 3; i++) {
+      sProvider.push(
+        <Box mx="3">
+          <Image
+            source={require("../../../assets/images/logo.png")}
+            resizeMode="contain"
+            alt="image"
+            size="40"
+            backgroundColor="gray.200"
+            borderRadius="20"
+          />
+          <Box>
+            <Text fontWeight="bold" fontSize="lg">
+              Provider A
+            </Text>
+            <Text fontWeight="thin">This could be a brief cr...</Text>
+          </Box>
+        </Box>
+      );
+    }
+    return sProvider;
+  };
+
+  const generateVerticalSProvider = () => {
+    let sProvider = [];
+    for (let i = 0; i < 3; i++) {
+      sProvider.push(
+        <HStack space="5" my="2" alignItems="center">
+          <Image
+            source={require("../../../assets/images/logo.png")}
+            resizeMode="contain"
+            alt="image"
+            size="40"
+            backgroundColor="gray.200"
+            borderRadius="20"
+          />
+          <Box>
+            <Text fontWeight="bold" fontSize="lg">
+              Provider A
+            </Text>
+            <Text fontWeight="thin">
+              This could be a longer version of the description of the service
+              provider.
+            </Text>
+          </Box>
+        </HStack>
+      );
+    }
+    return sProvider;
+  };
+
   return (
     <ScrollView mt="10" mx="5" showsVerticalScrollIndicator={false}>
       <VStack space="10">
         <Box>
           <Heading>Suggested</Heading>
-          <ScrollView horizontal={true} my="5">
-            <Box mx="3">
-              <Image
-                source={require("../../../assets/images/logo.png")}
-                resizeMode="contain"
-                alt="image"
-                size="32"
-                backgroundColor="gray.100"
-                borderRadius="20"
-              />
-              <Text fontWeight="bold" fontSize="lg">
-                Provider A
-              </Text>
-              <Text fontWeight="thin">Solar Panel...</Text>
-            </Box>
-            <Box mx="3">
-              <Image
-                source={require("../../../assets/images/logo.png")}
-                resizeMode="contain"
-                alt="image"
-                size="32"
-                backgroundColor="gray.100"
-                borderRadius="20"
-              />
-              <Text fontWeight="bold" fontSize="lg">
-                Provider A
-              </Text>
-              <Text fontWeight="thin">Solar Panel...</Text>
-            </Box>
-            <Box mx="3">
-              <Image
-                source={require("../../../assets/images/logo.png")}
-                resizeMode="contain"
-                alt="image"
-                size="32"
-                backgroundColor="gray.100"
-                borderRadius="20"
-              />
-              <Text fontWeight="bold" fontSize="lg">
-                Provider A
-              </Text>
-              <Text fontWeight="thin">Solar Panel...</Text>
-            </Box>
-            <Box mx="3">
-              <Image
-                source={require("../../../assets/images/logo.png")}
-                resizeMode="contain"
-                alt="image"
-                size="32"
-                backgroundColor="gray.100"
-                borderRadius="20"
-              />
-              <Text fontWeight="bold" fontSize="lg">
-                Provider A
-              </Text>
-              <Text fontWeight="thin">Solar Panel...</Text>
-            </Box>
-            <Box mx="3">
-              <Image
-                source={require("../../../assets/images/logo.png")}
-                resizeMode="contain"
-                alt="image"
-                size="32"
-                backgroundColor="gray.100"
-                borderRadius="20"
-              />
-              <Text fontWeight="bold" fontSize="lg">
-                Provider A
-              </Text>
-              <Text fontWeight="thin">Solar Panel...</Text>
-            </Box>
-            <Box mx="3">
-              <Image
-                source={require("../../../assets/images/logo.png")}
-                resizeMode="contain"
-                alt="image"
-                size="32"
-                backgroundColor="gray.100"
-                borderRadius="20"
-              />
-              <Text fontWeight="bold" fontSize="lg">
-                Provider A
-              </Text>
-              <Text fontWeight="thin">Solar Panel...</Text>
-            </Box>
+          <ScrollView horizontal={true} my="5" py="2">
+            {generateSProvider()}
           </ScrollView>
         </Box>
-        <Box>
+        <Box width="45%">
           <Heading>All</Heading>
-          <HStack space="2" my="2">
-            <Image
-              source={require("../../../assets/images/logo.png")}
-              resizeMode="contain"
-              alt="image"
-              size="32"
-              backgroundColor="gray.100"
-              borderRadius="20"
-            />
-            <Box>
-              <Text fontWeight="bold" fontSize="lg">
-                Provider A
-              </Text>
-              <Text fontWeight="thin">Solar Panel...</Text>
-            </Box>
-          </HStack>
-          <HStack space="2" my="2">
-            <Image
-              source={require("../../../assets/images/logo.png")}
-              resizeMode="contain"
-              alt="image"
-              size="32"
-              backgroundColor="gray.100"
-              borderRadius="20"
-            />
-            <Box>
-              <Text fontWeight="bold" fontSize="lg">
-                Provider A
-              </Text>
-              <Text fontWeight="thin">Solar Panel...</Text>
-            </Box>
-          </HStack>
-          <HStack space="2" my="2">
-            <Image
-              source={require("../../../assets/images/logo.png")}
-              resizeMode="contain"
-              alt="image"
-              size="32"
-              backgroundColor="gray.100"
-              borderRadius="20"
-            />
-            <Box>
-              <Text fontWeight="bold" fontSize="lg">
-                Provider A
-              </Text>
-              <Text fontWeight="thin">Solar Panel...</Text>
-            </Box>
-          </HStack>
-          <HStack space="2" my="2">
-            <Image
-              source={require("../../../assets/images/logo.png")}
-              resizeMode="contain"
-              alt="image"
-              size="32"
-              backgroundColor="gray.100"
-              borderRadius="20"
-            />
-            <Box>
-              <Text fontWeight="bold" fontSize="lg">
-                Provider A
-              </Text>
-              <Text fontWeight="thin">Solar Panel...</Text>
-            </Box>
-          </HStack>
-          <HStack space="2" my="2">
-            <Image
-              source={require("../../../assets/images/logo.png")}
-              resizeMode="contain"
-              alt="image"
-              size="32"
-              backgroundColor="gray.100"
-              borderRadius="20"
-            />
-            <Box>
-              <Text fontWeight="bold" fontSize="lg">
-                Provider A
-              </Text>
-              <Text fontWeight="thin">Solar Panel...</Text>
-            </Box>
-          </HStack>
+          {generateVerticalSProvider()}
         </Box>
       </VStack>
     </ScrollView>

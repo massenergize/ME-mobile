c7c992ac95031744efdb92c4d5b93d9acb051a6b
malder08 <39073505+malder08@users.noreply.github.com>
2023-06-19 15:49:18 -0600

    incorporated data into cards

--- frontend/components/Pages/TestimonialsPage/TestimonialsCards.js
+++ frontend/components/Pages/TestimonialsPage/TestimonialsCards.js
@@ -2,27 +2,30 @@ import { View } from "react-native";
 import React from "react";
 import { Box, HStack, VStack, Text, Pressable, Container, Image } from "native-base";
 
-function TestimonialCardWithPic({ navigation }) {
+function TestimonialCardWithPic({ navigation, data }) {
   return (
     <Pressable onPress={() => navigation.navigate("testimonial")}>
         <Box mx={3} mt={2} bg="white" borderRadius="xl" shadow={1} borderTopLeftRadius="lg" borderBottomLeftRadius="lg" overflow="hidden">
             <HStack alignItems="center">
-                <Container width="30%" maxHeight={160}>
-                <Image
-                    source={{
-                        uri: "https://massenergize-prod-files.s3.amazonaws.com/media/Acton_Boxborough__BioBlitz_2023_A-230529-160415.jpg",
-                    }}
-                    alt="Testimonial Image"
-                    w="full"
-                    resizeMode="cover"
-                    h="full"
-                />
+                <Container width="30%" maxHeight={185}> 
+                    <Image
+                        source={{
+                            // uri: "https://massenergize-prod-files.s3.amazonaws.com/media/Acton_Boxborough__BioBlitz_2023_A-230529-160415.jpg",
+                            uri: data.file.url
+                        }}
+                        alt={data.file.url}
+                        w="full"
+                        resizeMode="cover"
+                        h="full"
+                    />
                 </Container>
                 <Container p={3} width="70%">
-                    <Text bold fontSize="lg">Testimonial Title</Text>
-                    <Text fontSize="sm" color="#BAB9C0">By Person's Name | 11/12/21</Text>
-                    <Text fontSize="sm" color="primary.400">Action Associated With</Text>
-                    <Text isTruncated={true} noOfLines={3}>This would be the text of the testimonial. Users are able to share their personal stories, and other users are able to learn from their experience. In addition other users</Text>
+                    <Text bold fontSize="lg">{data.title}</Text>
+                    <Text fontSize="sm" color="#BAB9C0">By {data.preferred_name} | {data.created_at}</Text>
+                    {
+                        (data.action != null) ? <Text fontSize="sm" color="primary.400">{data.action.title}</Text> : <></>
+                    }
+                    <Text isTruncated={true} noOfLines={3}>{data.body}</Text>
                 </Container>
             </HStack>
         </Box>
@@ -30,15 +33,17 @@ function TestimonialCardWithPic({ navigation }) {
   );
 }
 
-function TestimonialCardWithoutPic({ navigation }) {
+function TestimonialCardWithoutPic({ navigation, data }) {
     return (
         <Pressable onPress={() => navigation.navigate("testimonial")}>
             <Box mx={3} mt={2} bg="white" borderRadius="xl" shadow={1}>
                 <Box p={3}>
-                    <Text bold fontSize="lg">Testimonial Title</Text>
-                    <Text fontSize="sm" color="#BAB9C0">By Person's Name | 11/12/21</Text>
-                    <Text fontSize="sm" color="primary.400">Action Associated With</Text>
-                    <Text isTruncated={true} noOfLines={3}>This would be the text of the testimonial. Users are able to share their personal stories, and other users are able to learn from their experience. In addition other users can do whatever</Text>
+                    <Text bold fontSize="lg">{data.title}</Text>
+                    <Text fontSize="sm" color="#BAB9C0">By {data.preferred_name} | {data.created_at}</Text>
+                    {
+                        (data.action != null) ? <Text fontSize="sm" color="primary.400">{data.action.title}</Text> : <></>
+                    }
+                    <Text isTruncated={true} noOfLines={3}>{data.body}</Text>
                 </Box>
             </Box>
         </Pressable>
--- frontend/components/Pages/TestimonialsPage/TestimonialsPage.js
+++ frontend/components/Pages/TestimonialsPage/TestimonialsPage.js
@@ -2,15 +2,27 @@ import { ScrollView, Text } from "react-native";
 import React from "react";
 import { TestimonialCardWithPic, TestimonialCardWithoutPic } from "./TestimonialsCards.js";
 import { Container } from "native-base";
+import testimonials from "./../../../data/testimonialsList.json";
+
+// const testimonials = require("./../../../data/testimonialsList.json")
 
 export default function TestimonialsPage({ navigation }) {
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
-      <TestimonialCardWithPic navigation={navigation}/>
-      <TestimonialCardWithoutPic navigation={navigation}/>
-      <TestimonialCardWithoutPic navigation={navigation}/>
-      <TestimonialCardWithPic navigation={navigation}/>
-      <TestimonialCardWithoutPic navigation={navigation}/>
+      {
+        testimonials.data.map((item, index) => {
+          if (item.file != null) {
+            return (
+              <TestimonialCardWithPic navigation={navigation} data={item} key={index}/>
+            )
+          }
+          else {
+            return (
+              <TestimonialCardWithoutPic navigation={navigation} data={item} key={index}/>
+            )
+          } 
+          })
+        }
       <Container h={10}/>
     </ScrollView>
   );

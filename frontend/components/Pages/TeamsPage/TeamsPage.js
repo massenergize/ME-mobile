eed26dd567c0a04654b3ffecb186e7d23b6dedd2
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2023-06-19 13:20:31 -0400

    Add TeamsList page

--- frontend/components/Pages/TeamsPage/TeamsPage.js
+++ frontend/components/Pages/TeamsPage/TeamsPage.js
@@ -1,10 +1,71 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { ScrollView, Text, VStack, Image, Flex, Box } from "native-base";
 
 export default function TeamsPage() {
+  const generateTeams = () => {
+    let teams = [];
+    for (let i = 0; i < 5; i++) {
+      teams.push(
+        <Flex
+          key={i}
+          direction="row"
+          borderRadius={"2xl"}
+          shadow="5"
+          backgroundColor={"white"}
+        >
+          {i % 2 == 0 ? (
+            <Image
+              source={require("../../../assets/images/team-1.jpeg")}
+              alt="image"
+              size="xl"
+              height="full"
+              borderLeftRadius="2xl"
+            />
+          ) : null}
+          <Box flexShrink={1} borderRightRadius="2xl" w="full">
+            <VStack space="3" p="4">
+              <Text fontSize="lg" fontWeight="bold">
+                Team {i + 1}
+              </Text>
+              <Text>Description of the team and what their purpose is.</Text>
+              <VStack>
+                <Flex direction="row" justifyContent={"space-between"}>
+                  <Text fontSize="sm" fontWeight="bold">
+                    65
+                  </Text>
+                  <Text fontSize="sm">Members</Text>
+                </Flex>
+                <Flex direction="row" justifyContent={"space-between"}>
+                  <Text fontSize="sm" fontWeight="bold">
+                    124
+                  </Text>
+                  <Text fontSize="sm">Actions Completed</Text>
+                </Flex>
+                <Flex direction="row" justifyContent={"space-between"}>
+                  <Text fontSize="sm" fontWeight="bold">
+                    1615.2
+                  </Text>
+                  <Text fontSize="sm">Number of Trees</Text>
+                </Flex>
+                <Flex direction="row" justifyContent={"space-between"}>
+                  <Text fontSize="sm" fontWeight="bold">
+                    5
+                  </Text>
+                  <Text fontSize="sm">Sub-teams</Text>
+                </Flex>
+              </VStack>
+            </VStack>
+          </Box>
+        </Flex>
+      );
+    }
+    return teams;
+  };
   return (
-    <View>
-      <Text>TeamsPage</Text>
-    </View>
+    <ScrollView>
+      <VStack space="5" p="5">
+        {generateTeams()}
+      </VStack>
+    </ScrollView>
   );
 }

e3419ee5a69cd4e4becdcf8844e7641ea7958ab3
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2023-06-21 09:28:22 -0400

    Fix width issue in service providers

--- frontend/components/Pages/ServiceProvidersPage/ServiceProvidersPage.js
+++ frontend/components/Pages/ServiceProvidersPage/ServiceProvidersPage.js
@@ -9,13 +9,14 @@ import {
   HStack,
   Pressable,
 } from "native-base";
+import Page from "../../Shared/Page";
 
 export default function ServiceProvidersPage({ navigation }) {
   const generateSProvider = () => {
     let sProvider = [];
     for (let i = 0; i < 3; i++) {
       sProvider.push(
-        <Box mx="3" key={i}>
+        <Box mx="3" width="40" key={i}>
           <Pressable
             onPress={() => navigation.navigate("serviceProviderDetails")}
           >
@@ -31,7 +32,9 @@ export default function ServiceProvidersPage({ navigation }) {
               <Text fontWeight="bold" fontSize="lg">
                 Provider A
               </Text>
-              <Text fontWeight="thin">This could be a brief cr...</Text>
+              <Text fontWeight="thin">
+                This could be a brief description of...
+              </Text>
             </Box>
           </Pressable>
         </Box>
@@ -53,7 +56,7 @@ export default function ServiceProvidersPage({ navigation }) {
             backgroundColor="gray.200"
             borderRadius="20"
           />
-          <Box>
+          <Box width="150">
             <Text fontWeight="bold" fontSize="lg">
               Provider A
             </Text>
@@ -69,19 +72,21 @@ export default function ServiceProvidersPage({ navigation }) {
   };
 
   return (
-    <ScrollView mt="10" mx="5" showsVerticalScrollIndicator={false}>
-      <VStack space="10">
-        <Box>
-          <Heading>Suggested</Heading>
-          <ScrollView horizontal={true} my="5" py="2">
-            {generateSProvider()}
-          </ScrollView>
-        </Box>
-        <Box width="45%">
-          <Heading>All</Heading>
-          {generateVerticalSProvider()}
-        </Box>
-      </VStack>
-    </ScrollView>
+    <Page>
+      <ScrollView mt="10" mx="5" showsVerticalScrollIndicator={false}>
+        <VStack space="10">
+          <Box>
+            <Heading>Suggested</Heading>
+            <ScrollView horizontal={true} my="5" py="2">
+              {generateSProvider()}
+            </ScrollView>
+          </Box>
+          <Box>
+            <Heading>All</Heading>
+            {generateVerticalSProvider()}
+          </Box>
+        </VStack>
+      </ScrollView>
+    </Page>
   );
 }

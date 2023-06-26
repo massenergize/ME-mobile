294359ddada1b0d13292c4f3231d1da1633b7297
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2023-06-26 09:52:12 -0400

    Fix alignment issue

--- frontend/components/Pages/EventsPage/EventsPage.js
+++ frontend/components/Pages/EventsPage/EventsPage.js
@@ -1,15 +1,15 @@
 import React from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import {
+  Center,
   Button,
   Text,
   Box,
   Image,
   Icon,
-  Heading,
   Flex,
   ScrollView,
-  Pressable,
+  AspectRatio,
 } from "native-base";
 import Page from "../../Shared/Page";
 
@@ -18,9 +18,11 @@ const EVENTS = [
     id: 1,
     title: "Nature of Acton and Boxborough 2023 - A BioBlitz",
     date: "June 15th, 4:00 AM - 11:00 PM",
-    location: "Conservations Lands, Parks, and Yards, Acton, MA",
+    location: "Hybrid",
     image:
       "https://massenergize-prod-files.s3.amazonaws.com/media/Acton_Boxborough__BioBlitz_2023_A-230529-160415.jpg",
+    can_rsvp: true,
+    is_shared: true,
   },
   {
     id: 2,
@@ -29,6 +31,8 @@ const EVENTS = [
     location: "Online",
     image:
       "https://massenergize-prod-files.s3.amazonaws.com/media/Acton_Clean_Energy_Coach-230216-135015.jpg",
+    can_rsvp: false,
+    is_shared: false,
   },
   {
     id: 3,
@@ -37,6 +41,8 @@ const EVENTS = [
     location: null,
     image:
       "https://massenergize-prod-files.s3.amazonaws.com/media/hello_april_Flyer_Landscape-230328-194333.jpg",
+    can_rsvp: false,
+    is_shared: true,
   },
 ];
 
@@ -47,74 +53,90 @@ export default function EventsPage({ navigation }) {
         {EVENTS.map((event) => (
           <Box
             key={event.id}
-            m="3"
-            height="200"
-            shadow="5"
+            mx="auto"
+            my="3"
+            maxW="80"
+            rounded="lg"
             backgroundColor="white"
-            borderRadius="2xl"
+            shadow="5"
           >
-            <Pressable onPress={() => navigation.navigate("eventDetails")}>
-              <Box borderRadius="2xl" overflow="hidden">
+            <Box pt="2">
+              <AspectRatio w="100%" ratio={16 / 9}>
                 <Image
-                  source={{ uri: event.image }}
+                  source={{
+                    uri: event.image,
+                  }}
                   alt="image"
-                  h="full"
-                  w="full"
-                  resizeMode="cover"
+                  resizeMode="contain"
                 />
-                <Box
-                  w="full"
-                  h="full"
+              </AspectRatio>
+              {event.is_shared && (
+                <Center
+                  bg="secondary.400"
+                  _text={{
+                    color: "white",
+                    fontWeight: "bold",
+                    fontSize: "xs",
+                  }}
                   position="absolute"
-                  backgroundColor="primary.400"
-                  opacity="60"
-                ></Box>
+                  px="5"
+                  py="1.5"
+                  rounded="full"
+                  right="5"
+                  top="2"
+                >
+                  SHARED
+                </Center>
+              )}
+            </Box>
+            <Flex
+              px="4"
+              pt="4"
+              flexDirection="row"
+              justifyContent="space-between"
+            >
+              <Text fontWeight="bold" fontSize="md" w="80%" mr="3">
+                {event.title}
+              </Text>
+              <Icon
+                as={FontAwesome}
+                name="arrow-right"
+                size="md"
+                color="primary.400"
+              />
+            </Flex>
+            <Flex
+              backgroundColor={"gray.100"}
+              flexDirection="row"
+              flexWrap={"wrap"}
+              justifyContent="space-between"
+              alignItems="center"
+              borderBottomRadius="lg"
+              overflow={"hidden"}
+            >
+              <Box pl="4" pr="4">
+                <Text fontSize="sm" color="primary.400">
+                  {event.date}
+                </Text>
               </Box>
-              <Button variant="ghost" position="absolute" top="5" right="2">
-                <Icon
-                  as={FontAwesome}
-                  name="angle-right"
-                  size={6}
-                  color="white"
-                />
-              </Button>
-              <Box p="5" position="absolute" bottom="0">
-                <Heading color="white">
-                  {event.title.length > 35
-                    ? event.title.slice(0, 35) + "..."
-                    : event.title}
-                </Heading>
-                <Flex flexDirection="row" alignItems="center" mt="2">
-                  <Icon
-                    as={FontAwesome}
-                    name="calendar-o"
-                    size={4}
-                    color="white"
-                    mr="2"
-                  />
-                  <Text color="white" fontWeight="bold">
-                    {event.date}
+              {event.can_rsvp ? (
+                <Box backgroundColor="primary.400" flexGrow={1}>
+                  <Button
+                    variant="ghost"
+                    _text={{ fontSize: "xs", color: "white" }}
+                    _loading={{ backgroundColor: "white" }}
+                  >
+                    RSVP
+                  </Button>
+                </Box>
+              ) : (
+                <Box py="2" pr="4">
+                  <Text fontSize="sm" color="primary.400">
+                    {event.location}
                   </Text>
-                </Flex>
-                <Flex flexDirection="row" alignItems="center" mt="2">
-                  <Icon
-                    as={FontAwesome}
-                    name="location-arrow"
-                    size={4}
-                    color="white"
-                    mr="2"
-                    alignSelf="flex-end"
-                  />
-                  <Text color="white" fontWeight="bold">
-                    {event.location
-                      ? event.location.length > 30
-                        ? event.location.slice(0, 30) + "..."
-                        : event.location
-                      : "N/A"}
-                  </Text>
-                </Flex>
-              </Box>
-            </Pressable>
+                </Box>
+              )}
+            </Flex>
           </Box>
         ))}
       </ScrollView>

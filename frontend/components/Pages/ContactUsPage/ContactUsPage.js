387740a1153cab7344af6fdc20abc5280b9f4bed
Maria Alder <maria.i.alder@gmail.com>
2023-07-05 11:50:50 -0600

    fixed scrolling issue

--- frontend/components/Pages/ContactUsPage/ContactUsPage.js
+++ frontend/components/Pages/ContactUsPage/ContactUsPage.js
@@ -8,8 +8,7 @@ export default function ContactUsPage({ navigation }) {
   return (
     <Page>
       <ScrollView showsVerticalScrollIndicator={false} px={3}>
-        {/* <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}> */}
-        <KeyboardAvoidingView behavior="padding">
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'position' : null} keyboardVerticalOffset={Platform.OS === 'ios' ? 100 : 0}>
         <VStack>  
           <Text bold fontSize="lg" mt={5}>Community Administrator</Text>
           <Text fontSize="lg">{communityInfo.data.admins[0].full_name}</Text>
@@ -22,36 +21,35 @@ export default function ContactUsPage({ navigation }) {
               variant="rounded"
               size="lg"
               placeholder="Name"
-            />
+              />
           </FormControl>
           <FormControl mt={3}>
             <Input
               variant="rounded"
               size="lg"
               placeholder="Email"
-            />
+              />
           </FormControl>
           <FormControl mt={3}>
             <Input
               variant="rounded"
               size="lg"
               placeholder="Subject"
-            />
+              />
           </FormControl>
-          <FormControl mt={3}>
-            <Input
-              borderRadius={25}
-              size="lg"
-              placeholder="Message"
-              textAlignVertical="top"
-              multiline={true}
-              height={40}
-            />
-          </FormControl>
+            <FormControl mt={3}>
+              <Input
+                borderRadius={25}
+                size="lg"
+                placeholder="Message"
+                textAlignVertical="top"
+                multiline={true}
+                height={40}
+              />
+            </FormControl>
           <Button mt={3} bg="primary.400" onPress={() => {}}>SEND MESSAGE</Button>
-          <Container height={100} />
         </VStack>
-        </KeyboardAvoidingView>
+      </KeyboardAvoidingView>
       </ScrollView>
     </Page>
   );

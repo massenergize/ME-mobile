b1c1bdcf5e40c85c64ca5ece69c53182b7432b5d
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2023-07-06 16:16:55 -0400

    Update configuration for Firebase JS SDK

--- frontend/config/firebaseConfig.js
+++ frontend/config/firebaseConfig.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: "massenergize-auth.firebaseapp.com",
-    databaseURL: "https://massenergize-auth.firebaseio.com",
-    projectId: "massenergize-auth",
-    storageBucket: "massenergize-auth.appspot.com",
-    messagingSenderId: "72842344535",
-    appId: "1:72842344535:web:9b1517b1b3d2e818",
-    measurementId: "G-G4YG94EJ8D",
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL:process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);

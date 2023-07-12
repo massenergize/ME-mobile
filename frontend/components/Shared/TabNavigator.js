5fc760aef542e801d3054c22e850b8fa4b5035d6
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2023-07-12 15:36:07 -0400

    redirect to dashboard instead of user profile

--- frontend/components/Shared/TabNavigator.js
+++ frontend/components/Shared/TabNavigator.js
@@ -1,25 +1,26 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import CommunityPage from "../Pages/CommunityPage/CommunityPage";
 import ActionsPage from "../Pages/ActionsPage/ActionsPage";
 import EventsPage from "../Pages/EventsPage/EventsPage";
 import UserProfilePage from "../Pages/UserProfilePage/UserProfilePage";
 import AboidersPage from "../Pages/ServiceProvidersPage/ServiceProvidersPage";
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import Ionicons from "react-native-vector-icons/Ionicons";
+import DashboardPage from "../Pages/UserProfilePage/DashboardPage";
 
 const Tab = createBottomTabNavigator();
 
 const tabBarLabels = {
-  "COMMUNITY": "Community",
-  "ACTIONS": "Actions",
-  "EVENTS": "Events",
-  "PROFILE": "Profile"
-}
+  COMMUNITY: "Community",
+  ACTIONS: "Actions",
+  EVENTS: "Events",
+  PROFILE: "Profile",
+};
 
 export default function TabNavigator({ route, navigation }) {
-  console.log(route.params)
+  console.log(route.params);
   const { community_id } = route.params;
 
   return (
@@ -30,14 +31,11 @@ export default function TabNavigator({ route, navigation }) {
 
           if (route.name === "COMMUNITY") {
             iconName = "home";
-          }
-          else if (route.name === "ACTIONS") {
+          } else if (route.name === "ACTIONS") {
             iconName = "flash";
-          }
-          else if (route.name === "EVENTS") {
+          } else if (route.name === "EVENTS") {
             iconName = "calendar";
-          }
-          else if (route.name === "PROFILE") {
+          } else if (route.name === "PROFILE") {
             iconName = "person-circle";
           }
 
@@ -46,14 +44,17 @@ export default function TabNavigator({ route, navigation }) {
         tabBarActiveTintColor: "#64B058",
         tabBarInactiveTintColor: "#B3B2BD",
         tabBarLabel: tabBarLabels[route.name],
-        headerShown: false
-      })
-    }
+        headerShown: false,
+      })}
     >
-      <Tab.Screen name="COMMUNITY" component={CommunityPage} initialParams={{community_id: community_id}}/>
+      <Tab.Screen
+        name="COMMUNITY"
+        component={CommunityPage}
+        initialParams={{ community_id: community_id }}
+      />
       <Tab.Screen name="ACTIONS" component={ActionsPage} />
       <Tab.Screen name="EVENTS" component={EventsPage} />
-      <Tab.Screen name="PROFILE" component={UserProfilePage} />
-  </Tab.Navigator>
-  )
+      <Tab.Screen name="PROFILE" component={DashboardPage} />
+    </Tab.Navigator>
+  );
 }

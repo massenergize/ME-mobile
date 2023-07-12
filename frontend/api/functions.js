1d53f20300d246736e898a17bb7f7caea05c8de4
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2023-07-12 09:38:46 -0400

    Update api call to use local API

--- frontend/api/functions.js
+++ frontend/api/functions.js
@@ -1,3 +1,4 @@
+import { IS_LOCAL } from "../config";
 import URLS from "./urls";
 
 /**
@@ -26,15 +27,14 @@ export async function apiCall(
     destinationUrl = destinationUrl.substring(1);
   }
 
-  // if (IS_LOCAL) {
-  //     destinationUrl = "api/" + destinationUrl;
-  //   }
+  if (IS_LOCAL) {
+    destinationUrl = "api/" + destinationUrl;
+  }
 
   // const authToken = get_cookie(new Cookies(), "token"); // This is needed because in tests, cypress doesnt pass the token directly in the headers
   // const authTokenInLocalStorage = localStorage.getItem(AUTH_TOKEN); // This is also only used in test. Its a fallback method to retrieve token
   const formData = new FormData();
 
-  console.log("Data to send", dataToSend);
   Object.keys(dataToSend).map((k) => formData.append(k, dataToSend[k]));
   formData.append("", "");
 
@@ -49,27 +49,14 @@ export async function apiCall(
   //     // Choose one, depends what you need.
   //     return false; // If you want to ignore the error and do something in a chained .then()
   // })
-  console.log(formData);
 
   try {
-    console.log("URL", `${URLS.ROOT}/${destinationUrl}`);
-    console.log("Form Data", formData, JSON.stringify(dataToSend));
     const response = await fetch(`${URLS.ROOT}/${destinationUrl}`, {
       credentials: "include",
       method: "POST",
       body: formData,
     });
-    // const response = await sendXmlHttpRequest(`${URLS.ROOT}/${destinationUrl}`, formData);
     const json = await response.json();
-    // if (relocationPage && json && json.success) {
-    //     window.location.href = relocationPage;
-    // } else if (!json.success) {
-    //     if (json.error === "session_expired") {
-    //         window.location.href = window.location;
-    //     } else {
-    //         console.log(destinationUrl, json.error);
-    //     }
-    // }
     return json;
   } catch (error) {
     const errorText = error.toString();

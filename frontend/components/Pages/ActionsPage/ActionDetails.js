c2326ba45cbdba2086769557e076ad14d71d72d4
Maria Alder <maria.i.alder@gmail.com>
2023-07-31 16:41:42 -0600

    action buttons

--- frontend/components/Pages/ActionsPage/ActionDetails.js
+++ frontend/components/Pages/ActionsPage/ActionDetails.js
@@ -161,76 +161,80 @@ export default function ActionDetails({ route, navigation }) {
         isActionLoading 
         ? <Spinner />
         :
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <VStack style={{ flex: 1 }}>
-            <Image
-              source={{
-                  // uri: "https://m.media-amazon.com/images/I/61JhlT09xiL._AC_SX679_.jpg",
-                uri: (action.image != null) ? action.image.url : null,
+        <View>
+
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <VStack style={{ flex: 1 }}>
+              <Image
+                source={{
+                    // uri: "https://m.media-amazon.com/images/I/61JhlT09xiL._AC_SX679_.jpg",
+                  uri: (action.image != null) ? action.image.url : null,
+                }}
+                m={3}
+                h={250}
+                // w={width}
+                alt="image"
+                // borderRadius="xl"
+                resizeMode="contain"
+            />
+              <Box bg="white" borderRadius="3xl" shadow={5} height="100%">
+                <VStack>
+                  <Text bold fontSize="2xl" m={4}>{action.title}</Text>
+                  <HStack alignItems="center" mx={4}>
+                    <Text bold fontSize="lg">Impact</Text>
+                    <Spacer />
+                    <Text fontSize="lg">{getMetric("Impact")}</Text>
+                  </HStack>
+                  <HStack alignItems="center" mx={4} mt={2} mb={4}>
+                    <Text bold fontSize="lg">Cost</Text>
+                    <Spacer />
+                    <Text fontSize="lg">{getMetric("Cost")}</Text>
+                  </HStack>
+                  <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} px={3}>
+                    <TabButton label="Description" name="description" />
+                    <TabButton label="Steps" name="steps" />
+                    <TabButton label="Deep Dive" name="deep_dive" />
+                    <TabButton label="Testimonials" name="testimonials" />
+                    <TabButton label="Service Providers" name="service_providers" />
+                    <Container width={5}></Container>
+                  </ScrollView>
+                  <Box m={15}>
+                    {renderTabContent()}
+                  </Box>
+                </VStack>
+                <Container
+                  // style={{ flexDirection: "row", position: "absolute", bottom: 35 }}
+                  // position="absolute"
+                  // bottom={15}
+                >
+                </Container>
+              </Box>
+            </VStack>
+            <Container height={20}></Container>
+          </ScrollView>
+          <HStack space={4} width="100%" justifyContent="center" position="absolute" bottom={8}>
+            <Button
+              size="md"
+              variant="solid"
+              _text={{
+                color: "white",
+                fontWeight: "bold",
               }}
-              m={3}
-              h={250}
-              // w={width}
-              alt="image"
-              // borderRadius="xl"
-              resizeMode="contain"
-          />
-            <Box bg="white" borderRadius="3xl" shadow={5} height="100%">
-              <VStack>
-                <Text bold fontSize="2xl" m={4}>{action.title}</Text>
-                <HStack alignItems="center" mx={4}>
-                  <Text bold fontSize="lg">Impact</Text>
-                  <Spacer />
-                  <Text fontSize="lg">{getMetric("Impact")}</Text>
-                </HStack>
-                <HStack alignItems="center" mx={4} mt={2} mb={4}>
-                  <Text bold fontSize="lg">Cost</Text>
-                  <Spacer />
-                  <Text fontSize="lg">{getMetric("Cost")}</Text>
-                </HStack>
-                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} px={3}>
-                  <TabButton label="Description" name="description" />
-                  <TabButton label="Steps" name="steps" />
-                  <TabButton label="Deep Dive" name="deep_dive" />
-                  <TabButton label="Testimonials" name="testimonials" />
-                  <TabButton label="Service Providers" name="service_providers" />
-                  <Container width={5}></Container>
-                </ScrollView>
-                <Box m={15}>
-                  {renderTabContent()}
-                </Box>
-              </VStack>
-              <Container
-                // style={{ flexDirection: "row", position: "absolute", bottom: 35 }}
-                position="absolute"
-                bottom={15}
-              >
-                {/* <Button
-                  size="md"
-                  variant="outline"
-                  _text={{
-                    color: "black",
-                    fontWeight: "bold",
-                  }}
-                  ref={buttonOne}
-                >
-                  Add to To-Do
-                </Button>
-                <Button
-                  size="md"
-                  variant="solid"
-                  _text={{
-                    color: "white",
-                    fontWeight: "bold",
-                  }}
-                  ref={buttonTwo}
-                >
-                  Done
-                </Button> */}
-              </Container>
-            </Box>
-          </VStack>
-        </ScrollView>
+            >
+              Add to To-Do
+            </Button>
+            <Button
+              size="md"
+              variant="solid"
+              _text={{
+                color: "white",
+                fontWeight: "bold",
+              }}
+            >
+              Done
+            </Button>
+          </HStack>
+        </View>
       }
     </Page>
   );

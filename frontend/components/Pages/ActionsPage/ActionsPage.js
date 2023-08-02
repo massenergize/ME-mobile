31fe457403b980da32f39316289b96f242570bea
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2023-08-02 12:03:32 -0400

    add loading spinner when actions' DOM is being rendered.

--- frontend/components/Pages/ActionsPage/ActionsPage.js
+++ frontend/components/Pages/ActionsPage/ActionsPage.js
@@ -1,6 +1,6 @@
-import React, { useContext } from "react";
-import { Text, StyleSheet } from "react-native";
-import { ScrollView, HStack } from "native-base";
+import { useContext, useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
+import { ScrollView, HStack, Text, Spinner, Center } from "native-base";
 
 import Page from "../../Shared/Page";
 import ActionCard from "./ActionCard";
@@ -9,34 +9,26 @@ import { getActionMetric } from "../../Shared/Utils";
 
 export default function ActionsPage({ navigation }) {
   const { actions } = useContext(CommunityContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (actions) {
+      setIsLoading(false);
+    }
+  }, []);
 
   return (
     <Page>
-      <ScrollView>
-        <Text style={styles.category}>Recommended</Text>
-        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-          <HStack space={2} justifyContent="center" mx={15} marginBottom={15}>
-            {actions.map((action, index) => {
-              return (
-                <ActionCard
-                  key={index}
-                  navigation={navigation}
-                  id={action.id}
-                  title={action.title}
-                  imgUrl={action.image?.url}
-                  impactMetric={getActionMetric(action, "Impact")}
-                  costMetric={getActionMetric(action, "Cost")}
-                />
-              );
-            })}
-          </HStack>
-        </ScrollView>
-        <Text style={styles.category}>High Impact</Text>
-        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-          <HStack space={2} justifyContent="center" mx={15} marginBottom={15}>
-            {actions
-              .filter((action) => getActionMetric(action, "Impact") === "High")
-              .map((action, index) => {
+      {isLoading ? (
+        <Center m="5">
+          <Spinner />
+        </Center>
+      ) : (
+        <ScrollView>
+          <Text style={styles.category}>Recommended</Text>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+            <HStack space={2} justifyContent="center" mx={15} marginBottom={15}>
+              {actions.map((action, index) => {
                 return (
                   <ActionCard
                     key={index}
@@ -49,33 +41,56 @@ export default function ActionsPage({ navigation }) {
                   />
                 );
               })}
-          </HStack>
+            </HStack>
+          </ScrollView>
+          <Text style={styles.category}>High Impact</Text>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+            <HStack space={2} justifyContent="center" mx={15} marginBottom={15}>
+              {actions
+                .filter(
+                  (action) => getActionMetric(action, "Impact") === "High"
+                )
+                .map((action, index) => {
+                  return (
+                    <ActionCard
+                      key={index}
+                      navigation={navigation}
+                      id={action.id}
+                      title={action.title}
+                      imgUrl={action.image?.url}
+                      impactMetric={getActionMetric(action, "Impact")}
+                      costMetric={getActionMetric(action, "Cost")}
+                    />
+                  );
+                })}
+            </HStack>
+          </ScrollView>
+          <Text style={styles.category}>Low Cost</Text>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+            <HStack space={2} justifyContent="center" mx={15} marginBottom={15}>
+              {actions
+                .filter(
+                  (action) =>
+                    getActionMetric(action, "Cost") === "$" ||
+                    getActionMetric(action, "Cost") === "0"
+                )
+                .map((action, index) => {
+                  return (
+                    <ActionCard
+                      key={index}
+                      navigation={navigation}
+                      id={action.id}
+                      title={action.title}
+                      imgUrl={action.image?.url}
+                      impactMetric={getActionMetric(action, "Impact")}
+                      costMetric={getActionMetric(action, "Cost")}
+                    />
+                  );
+                })}
+            </HStack>
+          </ScrollView>
         </ScrollView>
-        <Text style={styles.category}>Low Cost</Text>
-        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-          <HStack space={2} justifyContent="center" mx={15} marginBottom={15}>
-            {actions
-              .filter(
-                (action) =>
-                  getActionMetric(action, "Cost") === "$" ||
-                  getActionMetric(action, "Cost") === "0"
-              )
-              .map((action, index) => {
-                return (
-                  <ActionCard
-                    key={index}
-                    navigation={navigation}
-                    id={action.id}
-                    title={action.title}
-                    imgUrl={action.image?.url}
-                    impactMetric={getActionMetric(action, "Impact")}
-                    costMetric={getActionMetric(action, "Cost")}
-                  />
-                );
-              })}
-          </HStack>
-        </ScrollView>
-      </ScrollView>
+      )}
     </Page>
   );
 }

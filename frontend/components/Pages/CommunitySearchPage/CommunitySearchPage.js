bdd4d071352ed38e445032040c4f0f0d6783b77f
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2023-08-09 13:51:28 -0400

    fix scrollview's container alignment issue

--- frontend/components/Pages/CommunitySearchPage/CommunitySearchPage.js
+++ frontend/components/Pages/CommunitySearchPage/CommunitySearchPage.js
@@ -14,33 +14,13 @@ import {
   Pressable,
   Spinner,
   Slider,
-  View,
 } from "native-base";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 import Page from "../../Shared/Page";
-import SearchBar from "../../Shared/SearchBar";
 import CommunityCard from "./CommunityCard";
 import { apiCall } from "../../../api/functions";
 
-const filterOptions = [
-  {
-    value: "all",
-    label: "All",
-  },
-  {
-    value: "cities & towns",
-    label: "Cities & Towns",
-  },
-  {
-    value: "other communities",
-    label: "Other Communities",
-  },
-  {
-    value: "schools",
-    label: "Schools",
-  },
-];
 
 export default function CommunitySearchPage({ navigation }) {
   const [communities, setCommunities] = useState([]);
@@ -89,80 +69,79 @@ export default function CommunitySearchPage({ navigation }) {
           </Heading>
         </Center>
       </Box>
-      <Box backgroundColor="white" top="-5%" borderTopRadius="30">
-        <Box mx="5">
-          <VStack space="5" pt="10">
-            <HStack space="2" alignItems="center">
-              <Text>Communities near: </Text>
-              <Pressable onPress={() => setShowModal(true)}>
-                <HStack space="1" alignItems="center">
-                  <Text fontSize="2xl" fontWeight="bold" color="primary.400">
-                    {zipCode ? zipCode : "0000"}
-                  </Text>
-                  <Icon as={FontAwesome} name="pencil" color="primary.400" />
-                </HStack>
-              </Pressable>
+      <Box
+        backgroundColor="white"
+        top="-5%"
+        borderTopRadius="30"
+        flex="1"
+        px="5"
+        pt="10"
+      >
+        <HStack space="2" alignItems="center">
+          <Text>Communities near: </Text>
+          <Pressable onPress={() => setShowModal(true)}>
+            <HStack space="1" alignItems="center">
+              <Text fontSize="2xl" fontWeight="bold" color="primary.400">
+                {zipCode ? zipCode : "0000"}
+              </Text>
+              <Icon as={FontAwesome} name="pencil" color="primary.400" />
             </HStack>
-            <VStack>
-              <Text>Distance: {maxDistance} miles</Text>
-              <Slider
-                defaultValue={maxDistance}
-                step={5}
-                accessibilityLabel="max distance slider"
-                onChange={(value) => setMaxDistance(value)}
-                onChangeEnd={() => handleZipCodeSubmit()}
-                isDisabled={zipCode === ""}
-              >
-                <Slider.Track>
-                  <Slider.FilledTrack />
-                </Slider.Track>
-                <Slider.Thumb />
-              </Slider>
-            </VStack>
-            {/* <SearchBar filterOptions={filterOptions} /> */}
-            {/* Container for communities */}
-            {isLoading ? (
-              <Spinner />
+          </Pressable>
+        </HStack>
+        <VStack>
+          <Text>Distance: {maxDistance} miles</Text>
+          <Slider
+            defaultValue={maxDistance}
+            step={5}
+            accessibilityLabel="max distance slider"
+            onChange={(value) => setMaxDistance(value)}
+            onChangeEnd={() => handleZipCodeSubmit()}
+            isDisabled={zipCode === ""}
+          >
+            <Slider.Track>
+              <Slider.FilledTrack />
+            </Slider.Track>
+            <Slider.Thumb />
+          </Slider>
+        </VStack>
+        {/* Container for communities */}
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <ScrollView showsVerticalScrollIndicator={false}>
+            {communities.length === 0 ? (
+              <Text textAlign="center" color="muted.400">
+                Please enter a zip code to find communities.
+              </Text>
             ) : (
-              <View height="75%">
-                {communities.length === 0 ? (
-                  <Text textAlign="center" color="muted.400">
-                    Please enter a zip code to find communities.
-                  </Text>
-                ) : (
-                  <ScrollView>
-                    <Text textAlign="center" color="muted.400">
-                      Found {communities.length} communities.
-                    </Text>
-                    {communities
-                      // sort by geographically focused first, then by distance
-                      .sort((a, b) =>
-                        a.is_geographically_focused &&
-                        !b.is_geographically_focused
-                          ? -1
-                          : 1
-                      )
-                      .sort((a, b) => a.location?.distance - b.location?.distance)
-                      .map((community) => {
-                        return (
-                          <CommunityCard
-                            py="2"
-                            community={community}
-                            key={community.id}
-                            onPress={() =>
-                              navigation.navigate("drawer", {
-                                community_id: community.id,
-                              })
-                            }
-                          />
-                        );
-                      })}
-                  </ScrollView>
-                )}
-              </View>
+              <VStack space="5" p="5">
+                <Text textAlign="center" color="muted.400">
+                  Found {communities.length} communities
+                </Text>
+                {communities
+                  // sort by geographically focused first, then by distance
+                  .sort((a, b) =>
+                    a.is_geographically_focused && !b.is_geographically_focused
+                      ? -1
+                      : 1
+                  )
+                  .sort((a, b) => a.location?.distance - b.location?.distance)
+                  .map((community) => (
+                    <CommunityCard
+                      py="2"
+                      community={community}
+                      key={community.id}
+                      onPress={() =>
+                        navigation.navigate("drawer", {
+                          community_id: community.id,
+                        })
+                      }
+                    />
+                  ))}
+              </VStack>
             )}
-          </VStack>
-        </Box>
+          </ScrollView>
+        )}
       </Box>
       {/* Modal for inputting zip code */}
       <Modal isOpen={showModal} onClose={() => setShowModal(false)} size="lg">

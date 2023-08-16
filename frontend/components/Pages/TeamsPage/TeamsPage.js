8cca284a909394f05fa3259ba6f40588ddc4d675
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2023-08-16 10:35:51 -0400

    improve UI of team's card

--- frontend/components/Pages/TeamsPage/TeamsPage.js
+++ frontend/components/Pages/TeamsPage/TeamsPage.js
@@ -6,68 +6,72 @@ import {
   View,
   Pressable,
   Text,
-  Spacer
+  Spacer,
 } from "native-base";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import Page from "../../Shared/Page";
 import TeamCard from "./TeamCard";
 import { CommunityContext } from "../../Contexts/CommunityContext";
 
-
 export default function TeamsPage({ navigation }) {
-
   const { teams } = useContext(CommunityContext);
-  const [subteamsExpanded, setSubteamsExpanded] = useState({})
-  const [teamsList, setTeamsList] = useState([])
+  const [subteamsExpanded, setSubteamsExpanded] = useState({});
+  const [teamsList, setTeamsList] = useState([]);
 
   // get a list of teams where subteams are nested under their parent team
   const getTeams = () => {
-    teamsDict = {}
-    expanded = {}
+    teamsDict = {};
+    expanded = {};
     for (var i = 0; i < teams.length; i++) {
       if (teams[i].team.parent === null) {
-        teamsDict[teams[i].team.id] = teams[i]
-        teamsDict[teams[i].team.id].subteams = []
+        teamsDict[teams[i].team.id] = teams[i];
+        teamsDict[teams[i].team.id].subteams = [];
       }
     }
     for (var i = 0; i < teams.length; i++) {
       if (teams[i].team.parent) {
-        teamsDict[teams[i].team.parent.id].subteams.push(teams[i])
-        expanded[teams[i].team.id] = false
+        teamsDict[teams[i].team.parent.id].subteams.push(teams[i]);
+        expanded[teams[i].team.id] = false;
       }
     }
-    setTeamsList(Object.values(teamsDict))
-    setSubteamsExpanded(expanded)
-  }
+    setTeamsList(Object.values(teamsDict));
+    setSubteamsExpanded(expanded);
+  };
 
   // update the state of whether a team's subteams are expanded or not
   const changeExpanded = (team_id) => {
-    let copy = {...subteamsExpanded}
-    copy[team_id] = !copy[team_id]
-    setSubteamsExpanded(copy)
-  }
+    let copy = { ...subteamsExpanded };
+    copy[team_id] = !copy[team_id];
+    setSubteamsExpanded(copy);
+  };
 
   // get the list of teams on page load
   useEffect(() => {
-    getTeams()
-  }, [teams])
+    getTeams();
+  }, [teams]);
 
   return (
     <Page>
       <ScrollView showsVerticalScrollIndicator={false}>
         <VStack space={5} p={5}>
-          {
-            teamsList.map((team, i) => {
-              return <View key={i}>
-                <TeamCard navigation={navigation} team={team} isSubteam={false} />
+          {teamsList.map((team, i) => {
+            return (
+              <View key={i}>
+                <TeamCard
+                  navigation={navigation}
+                  team={team}
+                  isSubteam={false}
+                />
                 {
                   // display the subteams associated with a parent if expanded
-                  (team.subteams.length > 0) ? (
+                  team.subteams.length > 0 ? (
                     <View>
                       <Pressable onPress={() => changeExpanded(team.team.id)}>
                         <HStack mt={2} alignItems="center">
                           <Spacer />
-                          <Text color="primary.600" mr={1}>Expand Subteams</Text>
+                          <Text color="primary.600" mr={1}>
+                            Expand Subteams
+                          </Text>
                           <Ionicons
                             name={
                               subteamsExpanded[team.team.id]
@@ -78,24 +82,26 @@ export default function TeamsPage({ navigation }) {
                           />
                         </HStack>
                       </Pressable>
-                      {
-                        subteamsExpanded[team.team.id] ? (
-                          <VStack ml={5} space={3} mt={3}>
-                          {
-                            team.subteams.map((subteam, j) => {
-                              return <TeamCard key={j} navigation={navigation} team={subteam} isSubteam={true}/>
-                            })
-                          }
-                          </VStack>
-                        ) : null
-                      }
+                      {subteamsExpanded[team.team.id] ? (
+                        <VStack ml={5} space={3} mt={3}>
+                          {team.subteams.map((subteam, j) => {
+                            return (
+                              <TeamCard
+                                key={j}
+                                navigation={navigation}
+                                team={subteam}
+                                isSubteam={true}
+                              />
+                            );
+                          })}
+                        </VStack>
+                      ) : null}
                     </View>
-                  )
-                  : null
+                  ) : null
                 }
               </View>
-            })
-          }
+            );
+          })}
         </VStack>
       </ScrollView>
     </Page>
